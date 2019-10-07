717aa831f687fb2b614c7a1749fe70250a2ae71b
Oswaldo Leon <oscar.meza.leon@gmaill.com>
2019-10-07 10:30:43 -0600

    Look and feel
    Added look and feel in the mainapp package, in the BodegaApp.java file ( the one that contains the main and the whole menu object

--- src/mainapp/BodegaApp.java
+++ src/mainapp/BodegaApp.java
@@ -1,23 +1,27 @@
-
 package mainapp;
 
 import Menus.vntnMenu;
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 /**
  *
- * @author  Leon
+ * @author Leon
  */
 public class BodegaApp {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        vntnMenu marcoMenu = new vntnMenu(); 
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        vntnMenu marcoMenu = new vntnMenu();
         marcoMenu.ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        UIManager.setLookAndFeel(new WindowsLookAndFeel());
         marcoMenu.ventana.setSize(850, 500); // establece el tamaño del marco
         marcoMenu.ventana.setVisible(true); // muestra el marco
     }
-    
+
 }
